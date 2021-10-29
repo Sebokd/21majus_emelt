d254a20520f302191db4c68c0d84bef267a54655
Sebokd <64211141+Sebokd@users.noreply.github.com>
2021-10-29 19:47:05 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,120 +5,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2005_majus
+namespace _21majus_Pars2012
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            string[] sorok = File.ReadAllLines("lottosz.dat");
-            List<Szam> szamok = new List<Szam>();
-            int[] Ertek = new int[255];
-            int paros = 0;
-            int paratlan = 0;
-
-            foreach (string sor in sorok)
+            string[] sorok = File.ReadAllLines("Selejtezo2012.txt");
+            List<Versenyző> adatok = new List<Versenyző>();
+            foreach (string sor in sorok.Skip(1))
             {
-                //szamok.Add(new Szam(sor));
-                string[] s = sor.Split(' ');
-                int vege = s.Length;
+                adatok.Add(new Versenyző(sor));
+            }
 
-                for (int i = 0; i < vege; i++)//mindig az első ötöt írja felül, mert miután beolvassa a kövit, a for ciklusban reseteli az "i"-t
-                { 
-                    Ertek[i] = int.Parse(s[i]);
+            /* 5 */
+            int versenyzokSzama = adatok.Count();
+            Console.WriteLine($"5.feladat: Hány atléta adata található a forrásban? Válasz: {versenyzokSzama} fő");
 
-                    if (Ertek[i] % 2 == 0)
-                    {
-                        paros++;
-                    }
-
-                    if (Ertek[i] % 2 == 1)
-                    {
-                        paratlan++;
-                    }
+            /* 6 */
+            int nagydobas = 0;
+            for (int i = 0; i < versenyzokSzama; i++)
+            {
+                if (adatok[i].Dobas1 == 78 || adatok[i].Dobas1 > 78 || adatok[i].Dobas2 == 78 ||
+                    adatok[i].Dobas2 > 78 || adatok[i].Dobas3 == 78 || adatok[i].Dobas3 > 78)
+                {
+                    nagydobas++;
                 }
             }
- 
-            /* 1 - kérje be az 52.hét lottószámait */
-            Console.WriteLine("Adj meg 5 számot, szóközzel elválasztva!");
-            string valasz = Console.ReadLine();
+            Console.WriteLine($"6. Feladat: A 78,00 méter felett dobott, így automatikusan tovább jutott versenyzők száma: {nagydobas}");
 
-            /* 2- Rendezze a bekért számokat emelkedő sorrendbe, a rendezett számokat írja ki! */
-            string[] valaszok = valasz.Split(' ');
-            int h = valaszok.Length;
-            int[] tomb = new int[h];
-            for (int i = 0; i < h; i++)
-            {
-                int szam = int.Parse(valaszok[i]);
-                tomb[i] = szam;
-            }
-            Array.Sort(tomb);
-
-            foreach (var item in tomb)
-            {
-                Console.Write($"{item}" + " ");
-            }
-
-            /* 3- Kérjen be a felhasználótól egy egész számot 1-51 között! */
-            Console.WriteLine("\nAdj meg 1 db egész számot!");
-            int megadott = int.Parse(Console.ReadLine());
-
-            /* 4- Írja ki a képernyőre a bekért számnak megfelelő sorszámú hét lottószámait, a
-               lottosz.dat állományban lévő adatok alapján! */
-            Console.WriteLine($"\nA {megadott}. heti lottószámok a következőek voltak: {sorok[megadott]}");
-
-            /* 5. A lottosz.dat állományból beolvasott adatok alapján döntse el, hogy volt-e olyan
-            szám, amit egyszer sem húztak ki az 51 hét alatt! A döntés eredményét (Van/Nincs) írja ki
-            a képernyőre! 0-90 vannak a számok */
             
-         
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine($"{adatok[i].Nev}-- {adatok[i].Kód} -- legnagyobb dobása: {adatok[i].Eredmény}");
+            }
 
-            /* 6  A lottosz.dat állományban lévő adatok alapján
-             * állapítsa meg, hogy hányszor volt páratlan szám a
-             * kihúzott lottószámok között! Az eredményt a képernyőre írja ki! */
-            Console.WriteLine($"{paros} db páros van, és {paratlan} db páratlan van");
+            double max = 0;
+            int maxi = 0;
+            for (int i = 0; i < versenyzokSzama; i++)
+            {
+                if (adatok[i].Eredmény > max)
+                {
+                    max = adatok[i].Eredmény;
+                    maxi = i;
+                }
+            }
 
-            /* 7 Fűzze hozzá a lottosz.dat állományból beolvasott lottószámok után a felhasználótól
-            bekért, és rendezett 52. hét lottószámait, majd írja ki az összes lottószámot a lotto52.ki
-            szöveges fájlba! A fájlban egy sorba egy hét lottószámai kerüljenek, szóközzel elválasztva
-            egymástól!  */
-            //string valasz - beirt számok
+
+
+            Console.WriteLine($"9. Feladat: lejobb eredményt elért atléta: " +
+                $"\n Név: {adatok[maxi].Nev, 10} " +
+                $"\n Csoport: {adatok[maxi].Csoport,10}" +
+                $"\n Nemzet: {adatok[maxi].Nemzet,10}" +
+                $"\n Nemzet Kód: {adatok[maxi].Kód,10}" +
+                $"\n Sorozat: {adatok[maxi].Dobas1};{adatok[maxi].Dobas2};{adatok[maxi].Dobas3}" +
+                $"\n Eredmény: {adatok[maxi].Eredmény}");
+
+            /* 10 */
 
             List<string> ki = new List<string>();
-            foreach (string sorozatok in sorok)
+            ki.Add($"Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény");
+            for (int i = 0; i < 12; i++)
             {
-                ki.Add(sorozatok);
-            }
-            ki.Add(valasz);
-            File.WriteAllLines("lotto52.ki", ki, Encoding.UTF8);
-
-            /* 8. Határozza meg a lotto52.ki állomány adatai alapján, hogy az egyes számokat hányszor
-            húzták ki 2003-ban. Az eredményt írja ki a képernyőre a következő formában: az első sor
-            első eleme az a szám legyen ahányszor az egyest kihúzták! Az első sor második eleme az
-            az érték legyen, ahányszor a kettes számot kihúzták stb.! 
-            (Annyit biztosan tudunk az értékekről, hogy mindegyikük egyjegyű.)  */
-
-            string[] sorok2 = File.ReadAllLines("lotto52.ki");
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            for (int i = 0; i < 254; i++)
-            {
-                int kulcs = Ertek[i];
-                if (dict.ContainsKey(kulcs))
+                Versenyző legjobb = adatok.First();
+                foreach (var v in adatok.Skip(1))
                 {
-                    dict[kulcs]++;
+                    if (v.Eredmény > adatok[i].Eredmény) legjobb = v;
                 }
-                else
-                {
-                    dict.Add(kulcs, 1);
-                }
+                ki.Add($"{i};{legjobb.Nev};{legjobb.Csoport};{legjobb.Nemzet};{legjobb.Kód};{legjobb.Dobas1};{legjobb.Dobas2};{legjobb.Dobas3};{legjobb.Eredmény}");
+                adatok.Remove(legjobb);
             }
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"{item.Key}  { item.Value}");
-            }
+            File.WriteAllLines("Dontos2012.txt", ki, Encoding.UTF8);
 
-            Console.ReadKey();
+            Console.ReadLine();
 
         }
     }
